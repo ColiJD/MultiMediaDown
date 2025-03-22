3ae0fc2a5096bb97e44548ed290f07905540393a
ColiJD <colindresj9@gmail.com>
2025-03-22 14:22:07 -0600

    add ruta de carpeta

--- utils/downloader.py
+++ utils/downloader.py
@@ -1,6 +1,11 @@
 import yt_dlp
 from .progress_hook import progress_hook
+import os
 
+# Definir la ruta base y las subcarpetas
+DOWNLOAD_FOLDER = r"C:\AYVdownload"  # Ruta absoluta en Windows
+VIDEOS_FOLDER = os.path.join(DOWNLOAD_FOLDER, "videos")  # Subcarpeta para videos
+AUDIOS_FOLDER = os.path.join(DOWNLOAD_FOLDER, "audios")  # Subcarpeta para audios
 def download_video(link, progress_bar, download_type):
     """
     Descarga un video o solo el audio de YouTube usando yt-dlp.
@@ -12,13 +17,13 @@ def download_video(link, progress_bar, download_type):
     if download_type == 'video':
         ydl_opts = {
             'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',  # Descargar el mejor video y audio
-            'outtmpl': '%(title)s.%(ext)s',
+            'outtmpl': os.path.join(VIDEOS_FOLDER, '%(title)s.%(ext)s'),  # Guardar en la carpeta de videos
             'progress_hooks': [lambda d: progress_hook(d, progress_bar)],  # Hook de progreso
         }
     elif download_type == 'audio':
         ydl_opts = {
             'format': 'bestaudio/best',  # Descargar solo el mejor audio
-            'outtmpl': '%(title)s.%(ext)s',
+            'outtmpl': os.path.join(AUDIOS_FOLDER, '%(title)s.%(ext)s'),  # Guardar en la carpeta de audios
             'extractaudio': True,  # Extraer solo el audio
             'audioformat': 'mp3',  # Formato de salida del audio
             'progress_hooks': [lambda d: progress_hook(d, progress_bar)],  # Hook de progreso
@@ -29,6 +34,6 @@ def download_video(link, progress_bar, download_type):
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([link])
-        return f'{"Video" if download_type == "video" else "Audio"} descargado correctamente: {link}'
+        return f'{"Video" if download_type == "video" else "Audio"} descargado correctamente: {link}, '
     except Exception as e:
         raise Exception(f'Error durante la descarga de {link}: {str(e)}')
